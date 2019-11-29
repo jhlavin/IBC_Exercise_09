a6c794c1d11023457d9bc686d792c43b8a1496a5
John Hlavin <jhlavin@nd.edu>
2019-11-28 23:02:03 -0500

    completed part 1

--- exercise09.R
+++ exercise09.R
@@ -2,7 +2,15 @@ rm(list = ls())
 
 library(ggplot2)
 
-##Problem 2##
+## Problem 1 ##
+fish_data = read.csv("rec_fisheries.csv", header = T)
+
+ggplot(data = fish_data, mapping = aes(x = anglers, y = fish.harvested)) +
+  geom_point() + geom_smooth(method = lm, se = F) +
+  xlab("Recreational Fishermen") + ylab("Fish Harvested Recreationally") +
+  theme_minimal()
+
+## Problem 2 ##
 data = read.table("data.txt", sep = ",", header = T)
 
 #bar plot
@@ -14,3 +22,6 @@ ggplot(data = data, mapping = aes(x = region, y = observations)) +
 ggplot(data = data, mapping = aes(x = region, y = observations)) +
   geom_jitter(mapping = aes(color = region)) +
   theme_minimal()
+
+#the bar plot shows that the means of the four regions are almost the exact same but the scatter plot shows that
+#the distributions causing the same mean are very different
